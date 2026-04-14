c1825b3ec020343f41f8374368ae4aae64505d9b
pfeiffrk <pfeiffrk@gmail.com>
2026-04-14 19:23:13 -0400

    Configure Firebase deployment

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 export const firebaseProject = {
-  enabled: false,
+  enabled: true,
   config: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyBCEDsQiVGHTcGE03jS9FiN1vYGqTn7o_0",
+    authDomain: "dashboard-80124.firebaseapp.com",
+    projectId: "dashboard-80124",
+    storageBucket: "dashboard-80124.firebasestorage.app",
+    messagingSenderId: "820457289539",
+    appId: "1:820457289539:web:f16fd3d3d83872b80761f6"
   }
 };
